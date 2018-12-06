925754e04d32d3efeaa05a29136c8e278ad9bf51
Joshua Stoltenberg <stoltenberg96@gmail.com>
2018-12-06 10:22:58 -0600

    Initial commit, starting project from mosh tutorial

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import React, { Component } from "react";
+import "./App.css";
+
+class App extends Component {
+  render() {
+    return (
+      <main className="container">
+        <table className="table">
+          <thead>
+            <tr>
+              <th scope="col">Title</th>
+              <th scope="col">Genre</th>
+              <th scope="col">Stock</th>
+              <th scope="col">Rate</th>
+              <th scope="col" />
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <th scope="row" />
+              <td />
+            </tr>
+          </tbody>
+        </table>
+      </main>
+    );
+  }
+}
+
+export default App;
