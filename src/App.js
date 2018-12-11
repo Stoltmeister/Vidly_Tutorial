7dbeac0e5bdef0a91e4035e68d96e06e736d4b81
Joshua Stoltenberg <stoltenberg96@gmail.com>
2018-12-11 16:10:38 -0600

    Adding Routes, fixed bug where I had upper case 'component' in my routing

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React, { Component } from "react";
 import Movies from "./components/movies";
 import "./App.css";
+import { Route, Redirect, Switch, Link } from "react-router-dom";
+import Rentals from "./components/rentals";
+import NotFound from "./components/notFound";
+import Customers from "./components/customers";
 
 class App extends Component {
   render() {
     return (
       <main className="container">
-        <Movies />
+        <Route path="/movies" component={Movies} />
       </main>
     );
   }
