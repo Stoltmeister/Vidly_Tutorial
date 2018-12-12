abfab477af28d1190c65eb2f63b67fcb27d9d675
Joshua Stoltenberg <stoltenberg96@gmail.com>
2018-12-12 10:47:56 -0600

    Seperated most of the form code to a seperate component, making everything very modular and clean, also added register page/form

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Customers from "./components/customers";
 import NavBar from "./components/navBar";
 import MovieForm from "./components/movieForm";
 import LoginForm from "./components/loginForm";
+import RegisterForm from "./components/registerForm";
 import Movies from "./components/movies";
 import "./App.css";
 
@@ -16,6 +17,7 @@ class App extends Component {
         <NavBar />
         <main className="container">
           <Switch>
+            <Route path="/register" component={RegisterForm} />
             <Route path="/login" component={LoginForm} />
             <Route path="/movies/:id" component={MovieForm} />
             <Route path="/movies" component={Movies} />
